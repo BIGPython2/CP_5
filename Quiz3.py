d566c71e9f950263a6d4a7a61e3fe352af376e57
BIGPython2 <addpac3@gmail.com>
2023-09-22 16:18:36 +0900

    4주차 과제입니다.
    list와 for반복문을 활용하여 학생들의 점수를 학점표에 따라 분류하였습니다

---
+++ Quiz3.py
@@ -0,0 +1,12 @@
+student_score = [90,45,64,9,17,29]
+result = []
+for i in student_score:
+    if i >= 71:
+        result.append("A")
+    elif i >= 41:
+        result.append("B")
+    elif i >= 11:
+        result.append("C")
+    else:
+        result.append("D")
+print(result)
