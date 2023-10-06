c70f268a7280e7bbae856c6f13c544d4c889ae02
BIGPython2 <addpac3@gmail.com>
2023-10-06 21:07:02 +0900

    진짜 최종본 계산기
    네

--- QUIZ5.py
+++ QUIZ5.py
@@ -1,7 +1,7 @@
-def GGdan(n):
+def GGGdan(n):
     for x in range(1,n+1):
         print("------[" + str(x) + "단) ------")
         for y in range(1,x+1):
             print(x, "X", y, "=", x*y)
 num = int(input("몇 단까지 출력할까요?"))
-GGdan(num)
+GGGdan(num)
